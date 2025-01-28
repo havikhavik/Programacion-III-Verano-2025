3c24a4e96a1d5996e2dc7ba86e67e00324af673f
Carlos Nahuel Ramos Veron <carlosnahuelramosveron@MacBook-Air-de-Ana-3.local>
2025-01-28 20:05:42 -0300

    actividad 1

--- clase-2/actividad-1/src/App.java
+++ clase-2/actividad-1/src/App.java
@@ -3,15 +3,24 @@ public class App {
         actividad1();
     }
 
+    /**
+     * Por consenso 
+     * f(n) = 8n^2 + 5n + 8
+     * 8n^2 + 5n + 8 <= c n^2
+     * 8n^2/n^2 + 5n/n^2 + 8/n^2 <= c n^2/n^2
+     * 8 + 5 + 8 <= c
+     * 21 <= 21
+     * Por lo que f(n) = 8n^2 + 5n + 8 pertenece a O(n^2) para c = 21 y nO = 1
+     */
     static void actividad1() {
-        int[][] mat = {{4,5,6},{7,8,9},{5,6,7}}; 
-        int suma = 0;
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
+        int[][] mat = {{4,5,6},{7,8,9},{5,6,7}}; // 1
+        int suma = 0; // 1
+        for (int i = 0; i < mat.length; i++) { // 1 + 2(n+1) + n = 3 + 3n
+            for (int j = 0; j < mat.length; j++) { // 1 + 2(n+1) + n = 3 + 3n
                 suma += mat[i][j];
             }
         }
-        double promedio = (double) suma / (mat.length * mat.length);
-        System.out.println("Promedio: " + promedio);
+        double promedio = (double) suma / (mat.length * mat.length); // 2
+        System.out.println("Promedio: " + promedio); // 2
     }
 }
