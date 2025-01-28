66f7759f924870a5a07a3fb4b898acc183ce988a
Carlos Nahuel Ramos Veron <carlosnahuelramosveron@MacBook-Air-de-Ana-3.local>
2025-01-28 19:43:45 -0300

    actividad 1

--- clase-2/actividad-1/src/App.java
+++ clase-2/actividad-1/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        actividad1();
+    }
+
+    static void actividad1() {
+        int[][] mat = {{4,5,6},{7,8,9},{5,6,7}}; 
+        int suma = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                suma += mat[i][j];
+            }
+        }
+        double promedio = (double) suma / (mat.length * mat.length);
+        System.out.println("Promedio: " + promedio);
     }
 }
