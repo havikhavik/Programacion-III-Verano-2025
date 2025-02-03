5393fb8489ce2c70cfbf9c48d47aa7f1115d113d
Carlos Nahuel Ramos Veron <carlosnahuelramosveron@MacBook-Air-de-Ana-3.local>
2025-02-03 19:13:54 -0300

    actividad 1 clase 5

--- clase-5/actividad-1/src/App.java
+++ clase-5/actividad-1/src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Integer> monedas = List.of(10, 1, 5, 2, 10, 10, 5, 2, 5, 5, 5, 5, 5, 5, 10);
+        List<Integer> vuelto = actividad1(monedas, 35);
+        System.out.println(vuelto);
+    }
+
+    static List<Integer> actividad1(List<Integer> monedas, Integer vuelto) {
+        List<Integer> resultado = new ArrayList<>();
+        Collections.sort(monedas); //ordenar de mayor a menor
+        monedas.forEach(moneda -> {
+            if (moneda <= vuelto) {
+                resultado.add(moneda);
+            }
+        });
+        return resultado;
     }
 }
