35e43827bc61c7049d7073b0c7c93dfdb7d7a52a
Santiago Romano <santiiago.romano@gmail.com>
2025-02-05 21:20:22 -0300

    actividad 4 clase 7

--- clase-7/actividad-4/src/Actividad4Clase7.java
+++ clase-7/actividad-4/src/Actividad4Clase7.java
@@ -2,9 +2,30 @@ import java.util.*;
 
 public class Actividad4Clase7 {
 
+    public static int maxGanancia(int[] costos, int[] ganancias, int presupuesto) {
+        int n = costos.length;
+        int[][] dp = new int[n + 1][presupuesto + 1];
+
+        // Programación dinámica para maximizar la ganancia
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= presupuesto; j++) {
+                if (costos[i - 1] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - costos[i - 1]] + ganancias[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][presupuesto];
+    }
+
     public static void main(String[] args) {
-
-        System.out.println("Hola mundo ");
-
+        
+        int[] costos = {12, 20, 15, 25};
+        int[] ganancias = {150, 200, 100, 300};
+        int presupuesto = 35;
+        
+        System.out.println("Ganancia máxima obtenida: " + maxGanancia(costos, ganancias, presupuesto));
     }
 }
